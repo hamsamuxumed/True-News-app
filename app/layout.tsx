38f22f930fe373fea2d70ac94c46646d43e104c3
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2023-02-11 18:34:05 +0000

    add dark mode color

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body className="bg-gray-150 transition-all duration-700">
+      <body className="dark:bg-zinc-900 bg-gray-150 transition-all duration-700">
         <Header />
         <div className="max-w-6xl mx-auto">{children}</div>
       </body>
